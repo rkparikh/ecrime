7e2b10180553e6cfa4df92d6031859a03fddb37c
Rahul Parikh <rahulparikh.msu@gmail.com>
2016-05-24 23:40:12 +0530

    removed footer

--- code/ecrime_footer.php
+++ code/ecrime_footer.php
@@ -1,7 +1,6 @@
 <table width="100%" border="0">
 <tr>
 <td colspan="5"><div align="center">
-<span class="greysmall">(C) All Rights are Reserved with the Kogent Solutions Inc. </span></div>
 </td>
 </tr>
 </table>
